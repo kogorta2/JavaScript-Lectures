cd960668e94b8a0ba23e5ecdf4421ea174c52b83
Dmytro Obolonyk <dimonser147@gmail.com>
2016-12-13 21:29:15 +0200

    lecture1

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.jQuery
+"use strict";
+//alert("Hello World");
+// console.log("Hello World");
+// debugger;
+//var x = "My x";
+// var x = "My x";
+// x = "Another x";
+// console.log(x);
+// var x = "string";
+// console.log(
+    // typeof x
+// );
+//
+
+//var x = null;
+//console.log(typeof x);
+// var x = undefined;
+// console.log(typeof x);
+//var x = {};
+//console.log(typeof x);
+// var x = {
+	// key1:"value1"',
+	// key2:"value2",
+	// "key3":"value3"
+// };
+
+// x.key1 = "new value";
+// x["key2"] = "new value2";
+// var x = ["el","el1","el2",25,{},[],null];
+// console.log(x[3]); //25
+// x[3]=56;
+//x.test=123;
+// console.log(x[3]);
+// console.log(typeof x);
+
+// console.log(Array.isArray(x)); //true
+
+// var x=function(){};
+// x.key1="test";
+// console.log(x.key1); //"test"
+// console.log(typeof x); //"function"
+var x=["123"];
+console.log(x); //123
+var y=x;
+console.log(y); //123
+y[0]=345;
+console.log(y); //345
+console.log(x); //345
