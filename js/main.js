cbcf16b93621e455b1626d36044aa25c31d22896
Dmytro Obolonyk <dimonser147@gmail.com>
2016-12-15 21:25:06 +0200

    Lecture_2

---
+++ js/main.js
@@ -0,0 +1,205 @@
+// console.log("2+2*2",2+2*2); //6
+// console.log("5-3=",5-3); //2
+// console.log("2+2=",2+2); //4
+// console.log("5*3=",5*3); //15
+// console.log("5%3=",5%3); //2
+// var x = 25;
+// console.log("-x=",-x); //-25
+// var y = "25";
+// console.log("y=",y); //25
+// console.log("typeof y",typeof y);
+// console.log("+y",+y);//25
+// console.log("typeof y",typeof +y);//number
+
+// var z = '10';
+// z=+z;
+// console.log("typeof z",typeof z);//number
+
+// console.log("5<3",5<3);//false
+// console.log("5>3=",5>3);//true
+// console.log("3>=3",3>=3);//true
+// console.log("3<=3",3<=3);//true
+// console.log("5!='5'=",5!='5');//false
+// console.log("5!=5=",5!=5);//false
+// console.log("5!=='5'=",5!=='5');//true
+// console.log("null==underfined=",null==undefined);//true
+// console.log("null===underfined=",null===undefined);//false
+
+// function test()
+// {
+	// return("test success");
+// }
+
+// console.log(true&&test()&&"test"&&"end of ands list");//end of ands list
+// console.log(true&&test()&&0&&"end of ands list");//0
+// console.log(0&&6);//0
+
+// console.log(false||6);//6
+// console.log(true||6);//true
+// console.log(false||"end"||0);//"end"
+// console.log("start"||true||0);//"start"
+
+// console.log(!0);//true
+// console.log(!"");//true
+// console.log('!"true becouse contains text"',!"true becouse contains text");//false
+// console.log("!true",!true);//false
+// console.log("string",!!"string");//true
+
+// var x = (3,4,5);
+// console.log(x);//5
+
+// var x = 10,y=5,z=x+y;
+// console.log(z);//15
+// console.log(undefined +25);
+// console.log(NaN==NaN);
+// console.log(NaN===NaN);
+// console.log(typeof NaN);
+//console.log("2*(2+3)=",2*(2+3));//10
+
+// var x=10;
+// x++;//10
+// console.log(x);//11
+// console.log(x++);//11
+
+// var y=10;
+// ++y;//11
+// console.log(y);//11
+// console.log(++y);//12
+// var x=10;
+ // x--;//10
+ // console.log(x);//9
+ // console.log(x--);//10
+
+ // var y=10;
+ // --y;//9
+ // console.log(y);//9
+ // console.log(--y);//8
+
+// console.log("'x'>'y'",'x'>'y');//false
+// console.log("'x'>'Y'",'x'>'Y');//true
+// console.log("'test'>'tes'",'test'>'tes');//true
+// console.log("'test'>'zes'",'test'>'zes');//false
+
+// console.log("'1'>'2'",'1'>'2');//false
+// console.log("'12'>'2'",'12'>'2');//false
+// console.log("+'12'>+'2'",+'12'>+'2');//false
+
+// console.log("'2'>1",'2'>1);//true
+// console.log("'1.0'==1",'1.0'==1);//true
+// console.log('true==1',true==1);//true
+// console.log('false==0',false==0);//true
+// console.log("false==0&&false==''",false==0&&false=="");//true
+
+
+// console.log("null==undefined",null==undefined);//true
+// console.log("null>0",null>0);//false
+// console.log("null==0",null==0);//false
+// console.log("null<0",null<0);//true
+// console.log("null>=0",null>=0);//true
+// console.log("null<=0",null<=0);//true
+
+// true=!"",!0,!false,!null,!undefined,!NaN,{},[],function(){};
+// false=="",0,false,null,undefined,NaN;
+
+// if(){
+	// console.log(true)
+// }
+
+// if(true)
+	// console.log("1");
+
+// if(25&&0||"text")
+	// console.log("succes");
+
+// else
+	// console.log("not succes");
+
+// if(25&&0||""){
+// console.log("succes");
+// }
+
+// else{
+// console.log("not succes");
+// }
+// if(25&&0||false){
+// console.log("succes");
+// }
+
+// else{
+// console.log("not succes");
+// }
+
+// var userName=325;
+// if(userName=="Name"){
+	// alert("Hello admin");
+// }
+// else if (typeof +userName=="string"){
+	// alert("Hello user"+userName);
+// }
+// else {
+	// alert("You enter not a name");
+// }
+
+// var x=/*if*/10>15?/*then*/"test":/*else*/"rest";
+// console.log(x);
+// var x=/*if*/10<15?/*then*/"test":/*else*/"rest";
+// console.log(x);
+
+// if(10>15){
+	// var x="test";
+	// console.log(x);
+// }else{
+	// var x="rest";
+	// console.log(x);
+// }
+
+// var z=10>15?"test":true?"last":"best";
+// console.log(z);
+
+// var x="25";
+// switch(x){
+	// case"text":
+	// console.log("Action1");
+	// break;
+	// case 25:
+	// console.log("Action2");
+	// break;
+	// case true:
+	// console.log("Action3")
+	// default:
+	// console.log("Default");
+// }
+
+// var x="25";
+// if(typeof x=="string"&&x>=25||x!="some text"){
+	// console.log("Action1");
+// }
+
+// var x=[];
+// if(typeof x=="array"&&x[0]===undefined){
+	// console.log("Action1");
+// }else{
+	// console.log("Action2");
+// }
+
+// var x=[];
+// if(x[]===undefined){
+	// console.log("Action1");
+// }else{
+	// console.log("Action2");
+// }
+
+
+var x=[];
+if(x[]===undefined){
+	console.log("Action1");
+}else{
+	console.log("Action2");
+}
+
+
+
+
+
+
+
