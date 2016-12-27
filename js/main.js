b9737917e52e9a0b28a125aea36c08e88db4f80e
kogorta2 <kogorta2@.gmail.com>
2016-12-27 14:24:29 +0200

    last part

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 ﻿//1
-// var x='123';
+// var x='1';
 // var y=10;
-// var z=100;
-// console.log(!!x||y>=z); 
+// var z=10;
+// console.log(typeof x=='string'&&y==10&&z>=y); 
 
 //2
 // var x=11;
@@ -86,9 +86,68 @@
 // }
 // console.log(x);
 
+//9
+// var arr=['','0',0,true,undefined,NaN,234,'ololo',-1,[],{}];
+// var arrTrue=[];
+// for(var i=0;i<arr.length;i++){
+	// if(!!arr[i]===true){
+		// arrTrue.push(arr[i]);
+	// }
+// }
+// console.log('arrTrue=');
+// for(var j=0;j<arrTrue.length;j++){
+	// console.log(arrTrue[j]);
+	// }
 
-
-
+//10
+// var arr=['0',0,undefined,234,15,-29,'-287','',[1,2,3],{key:'value'},'test,false','14'];
+// var arrNumber=[];
+// for(var i=0;i<arr.length;i++){
+	// if((typeof arr[i]=='number'||typeof +arr[i]=='number')&&arr[i]>0){
+		// arrNumber.push(arr[i]);
+	// }
+// }
+// console.log('arrNumber=');
+// for(var j=0;j<arrNumber.length;j++){
+	// console.log(arrNumber[j]);
+// }	
+	
+//11
+var baseArray=[];
+for(j=0;j<99;j++){
+	baseArray.push(j+1);
+}
+var newArray1=[];
+var newArray2=[];
+var newArray3=[];
+for(var i=0;i<baseArray.length;i++){
+	newArray1.push(baseArray[i]);
+	i++;
+	newArray2.push(baseArray[i]);
+	i++;
+	newArray3.push(baseArray[i]);
+	}
+console.log('newArray1=');	
+for(var a=0;a<newArray1.length;a++){
+	console.log(newArray1[a]);
+}
+console.log('newArray2=');	
+for(var b=0;b<newArray2.length;b++){
+	console.log(newArray2[b]);
+}
+console.log('newArray3=');	
+for(var c=0;c<newArray3.length;c++){
+	console.log(newArray3[c]);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
