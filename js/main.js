31d5044e8fb4b7a7b084b65d9a52b2df17099c14
kogorta2 <kogorta2@gmail.com>
2017-01-10 12:41:10 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+﻿// function forSummator(x,y){
+	// var s=0;
+	// for(x;x<=y;x++){
+		// s=s+x;
+	// }
+// return s;
+// }
+// console.log(forSummator(2,6));
+
+function positiveMultiplier(x,y,z){
+	var m=1;
+	for(y;y<=z;y+x){
+		m=m*y;
+		// y=y+x;
+	}
+	return m;
+}
+console.log(positiveMultiplier(2,3,9));
