e51100db000695f85bcd802d1c0879d01a8511ed
kogorta2 <kogorta2@gmail.com>
2017-01-10 16:34:11 +0200

    second

--- js/main.js
+++ js/main.js
@@ -7,12 +7,63 @@
 // }
 // console.log(forSummator(2,6));
 
-function positiveMultiplier(x,y,z){
-	var m=1;
-	for(y;y<=z;y+x){
-		m=m*y;
+// function positiveMultiplier(x,y,z){
+	// var m=1;
+	// while(y<=z){
+		// if(y>0){m=m*y;}
 		// y=y+x;
-	}
-	return m;
-}
-console.log(positiveMultiplier(2,3,9));
+	// }
+	// return m;
+// }
+// console.log(positiveMultiplier(2,-3,9));
+
+// function arraySummator(a=[]){
+	// var s=0;
+	// for(i=0;i<a.length;i++){
+		// s=s+a[i];
+	// }
+	// return s;
+// }
+// console.log(arraySummator([-1,2,6,5]));
+
+// function arrayToConcatString(a=[]){
+	// for(i=0;i<a.length;i++){
+		// if(typeof a[i]==='string'){var s=s+a[i];}
+		// else continue;
+	// }
+	// return s;
+// }
+// var f=[-1,'1','string'];
+// console.log(arrayToConcatString(f));
+
+// function getPositionOfFive(a=[[],[],[]]){
+	// var step=0;
+	// for(i=0;i<a.length;i++){
+		// for(j=0;j<a[i].length;j++){
+			// if(a[i][j]===5){
+				// step=i*a[i].length+(j+1);
+				// return step;
+				
+			// }
+			
+		// }
+	// }
+	
+// }
+// console.log(getPositionOfFive([[7,4,8],[3,2,1],[5,9,6]]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
