63d28b7dc5d70ad4bd97a349c95d68b9503b0165
kogorta2 <kogorta2@gmail.com>
2017-01-12 16:04:19 +0200

    fibonachi

--- js/main.js
+++ js/main.js
@@ -1,69 +1,14 @@
-﻿// function forSummator(x,y){
-	// var s=0;
-	// for(x;x<=y;x++){
-		// s=s+x;
-	// }
-// return s;
-// }
-// console.log(forSummator(2,6));
-
-// function positiveMultiplier(x,y,z){
-	// var m=1;
-	// while(y<=z){
-		// if(y>0){m=m*y;}
-		// y=y+x;
-	// }
-	// return m;
-// }
-// console.log(positiveMultiplier(2,-3,9));
-
-// function arraySummator(a=[]){
-	// var s=0;
-	// for(i=0;i<a.length;i++){
-		// s=s+a[i];
-	// }
-	// return s;
-// }
-// console.log(arraySummator([-1,2,6,5]));
-
-// function arrayToConcatString(a=[]){
-	// for(i=0;i<a.length;i++){
-		// if(typeof a[i]==='string'){var s=s+a[i];}
-		// else continue;
-	// }
-	// return s;
-// }
-// var f=[-1,'1','string'];
-// console.log(arrayToConcatString(f));
-
-// function getPositionOfFive(a=[[],[],[]]){
-	// var step=0;
-	// for(i=0;i<a.length;i++){
-		// for(j=0;j<a[i].length;j++){
-			// if(a[i][j]===5){
-				// step=i*a[i].length+(j+1);
-				// return step;
-				
-			// }
-			
-		// }
-	// }
-	
-// }
-// console.log(getPositionOfFive([[7,4,8],[3,2,1],[5,9,6]]));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+﻿function summOfFibonachi(n){
+	var members=[];
+	members[0]=0;
+	members[1]=1;
+	for(var i=2;i<=n;i++){
+	members[i]=members[i-1]+members[i-2];
+		}	
+	var summ=0;
+	for(var j=0;j<=n;j++){
+		summ+=members[j];
+	}
+	return summ;
+}
+console.log(summOfFibonachi(4));
