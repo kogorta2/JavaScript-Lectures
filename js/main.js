331067430b2c4909a8530d4f2267a0cb1343d336
Dmytro Obolonyk <dimonser147@gmail.com>
2017-01-14 13:31:30 +0200

    Lecture_8

---
+++ js/main.js
@@ -0,0 +1,74 @@
+ function getRandomInt(min,max){
+	 return Math.floor(Math.random()*(max-min+1))+min;
+ }
+// function addOpacity(hexColor,opacity){
+	// var colorR=string.slice(1,3);
+	// var colorG=string.slice
+	// var rgbaColor=parseInt(hexColor,16);
+// }
+
+// function blockGrower(width,height){
+	
+	// var w=parseFloat(width);
+	// var h=parseFloat(height);
+	// var s1=String(h);
+	// var s2=String(w);
+	// var l1=s1.length;
+	// var l2=s2.length;
+	// var px1=width.substring(l1);
+	// var px2=height.substring(l2);
+	// w=(1.5*w).toFixed(4);
+	// h=(1.5*h).toFixed(4);
+	// return {width:w+px1,height:h+px2};
+// }
+
+// console.log(blockGrower("10 px","20 px"));
+
+// function getTwoNumbers(min,max){
+	// var a=[];
+	// for(var i=0;i<5;i++){
+		// a[i]=getRandomInt(min,max);
+	// }
+	// var sortA=a.sort();
+	// return {"min":sortA[0],"max":sortA[4]};
+// }
+// console.log(getTwoNumbers(24,48));
+
+// function getRandomPart(string){
+	// var l=string.length;
+	// string=string.substring(getRandomInt(0,string.length),getRandomInt(0,string.length));
+	// return console.log(getRandomInt(0,l)+string+getRandomInt(0,l));
+// }
+// console.log(getRandomPart("fufafefv"));
+
+// function getTwoSymbols(string){
+	// var s1=string[getRandomInt(0,string.length-1)];
+	// var s2=string[getRandomInt(0,string.length-1)];
+	// if ((s1.toLowerCase)!==(s2.toLowerCase)){
+		// s1=s1+s2;
+		// return s1;
+		// }
+	// else{return s1;}
+// }
+// console.log(getTwoSymbols("DGDHHJgdudjj"));
+
+function wordRemover(word,string){
+	var a=string.split("word");
+	
+	return a;
+}
+console.log(wordRemover("word","There ara two words: word, word, sort"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
