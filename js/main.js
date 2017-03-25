6f11258290bc02888b9651db7f50e03058528c1d
Dmytro Obolonyk <dimonser147@gmail.com>
2017-03-25 13:24:22 +0200

    "Angular part2"

--- js/main.js
+++ js/main.js
@@ -46,9 +46,34 @@
 		myCtrl.html=$sce.trustAsHtml("<h1>test text</h1>");
 		myCtrl.html2=$sce.trustAsHtml($templateCache.get("myTemplate.html"));
 		myCtrl.timer=new Date().getSeconds();
+		myCtrl.date=$.now();
+		myCtrl.usersFilter="";
+		myCtrl.names=[];
+		myCtrl.selectedOption="";
+		myCtrl.imageSrc="";
 		
+		myCtrl.options={
+			{value:"",lable:"Select item"},
+			{value:"item1",lable:"Select item1"},
+			{value:"item2",lable:"Select item2"},
+			{value:"item3",lable:"Select item3"},
+			{value:"item4",lable:"Select item4"},
+			{value:"item5",lable:"Select item5"},
+			{value:"item6",lable:"Select item6"},
+			
+		}
 		
+		myCtrl.onBlur=onBlur;
 		myCtrl.askUser=askUser;
+		myCtrl.usersFilterMethod=usersFilterMethod;
+		
+		function onBlur($event){
+			console.log($event);
+		}
+		
+		function usersFilterMethod(elem, index, array){
+			return ~elem.name.toLowerCase().indexOf(myCtrl.usersFilter.toLowerCase());
+		}
 		
 		$scope.$applyAsync(function(){
 			myCtrl.serverData={"myKey1":"asdasd"};
@@ -64,6 +89,7 @@
 		function askUser(){
 			myModal().then(function(userAnswer){
 				myCtrl.userAction=userAnswer;
+				myCtrl.imageSrc=imageSrc;
 			})
 		}
 		
